1bad0751f4d3d9945ec39e9c3f2f85ea536f0f7c
Jawid Olmas <jawidbay@gmail.com>
2026-07-05 16:17:57 +0300

    Updating same file using git bash with commands

---
+++ Arrays/new-file-via-git-bash.cpp
@@ -0,0 +1,4 @@
+// This is a new file created using git bash commands for practice. 
+// Before this, I was using the github website and desktop and do all commits and pushes or any other updates using GUI.
+// As things went deep, it was crucial to do things using commands in order to be as fast and convenient as possible
+// Commited on 7 of July 2026. 16:16 on Sunday
