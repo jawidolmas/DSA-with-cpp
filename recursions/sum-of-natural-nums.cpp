5f52aa1901df13f2f806228c35f4caa3b872c484
Jawid Olmas <jawidbay@gmail.com>
2026-07-09 15:25:27 +0300

    Improving code and solving the bug of i

--- recursions/sum-of-natural-nums.cpp
+++ recursions/sum-of-natural-nums.cpp
@@ -29,7 +29,7 @@ int usingRecursion(int n){
 // This method is second fastest, it takes 3 int variables so its O(1) for space. and for time it is O(n), as shown below in function;
 int usingLoop(int n){
     int i, s=0;             // 1 step
-    for(i = 0; i<=n; i++){ // Usually, a loop is n+1 steps;
+    for(i = 1; i<=n; i++){ // Usually, a loop is n+1 steps;
         s = s + i;         // it does n steps, on each iteration of i, it sums up. 
     }
     return s;             // 1 step
