28094b85cc7e28ae2edeb4fdfd8504498ff112ba
Jawid Olmas <jawidbay@gmail.com>
2026-07-19 17:37:03 +0300

    New tests performed

--- libraries/array/main.cpp
+++ libraries/array/main.cpp
@@ -8,9 +8,10 @@ using namespace std;
 int main(){
 
     Array arr(5);
-    arr.append(15);
+    arr.append(20);
     arr.append(16);
     arr.append(17);
+    arr.append(18);
     arr.insert(0, 15);
     arr.display();
     arr.Delete(6);
@@ -21,4 +22,14 @@ int main(){
     cout << arr.sum() <<endl;
     cout << arr.average() << endl;
     arr.linearsearch(20);
+    cout << arr.get(4) << endl;
+    arr.set(4, 9);
+    arr.transpositionLinearSe(9);
+    arr.display();
+    arr.transpositionLinearSe(9);
+    arr.display();
+    arr.movetoFrontLinearSe(17);
+    arr.display();
+    arr.movetoFrontLinearSe(17);
+    arr.display();
 }
