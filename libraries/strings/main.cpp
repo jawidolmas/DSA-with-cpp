aec2a89dc0d6fd79ff469346536a3270f99ed5a7
Jawid Olmas <jawidbay@gmail.com>
2026-07-27 17:26:28 +0300

    New functions tested in main

--- libraries/strings/main.cpp
+++ libraries/strings/main.cpp
@@ -1,27 +1,25 @@
 #include<iostream>
 #include "string.hpp"
-
-
-/*
-        Display
-        Length
-        Is Empty
-        Is Full
-        Clear
-
-
-*/
-
-
+using namespace std;
 
 
 int main()
 {
-    MyString s1("Hello");
+    MyString s1("__init__");
+    MyString s2("Jawid");
     s1.display();
-    std::cout << s1.length();
+    std::cout << s1.length() << endl;
+    s1.ToggleCase();
+    cout << endl;
+    cout << s1.isAlphabetic();
+    cout << s1.isNumeric();
+    cout << s1.hasAtleastOneChar();
+    cout << s1.hasLeastOneNum();
+    cout << endl;
+    cout << s1.isValidIdentifier() << endl;
+    s1.compareStrings(s1, s2);
     
 
 
-
+    return 0;
 }
