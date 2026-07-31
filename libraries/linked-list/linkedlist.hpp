844ea7f0ccb1f9fc220c605ae2cddc1bf69fe96d
Jawid Olmas <jawidbay@gmail.com>
2026-07-31 15:34:35 +0300

    Update library with new functions signature

--- libraries/linked-list/linkedlist.hpp
+++ libraries/linked-list/linkedlist.hpp
@@ -9,4 +9,21 @@ class linkedlist{
     public:
         linkedlist();
         ~linkedlist();
+        int  length() const;
+        void display() const;
+        bool isEmpty() const;
+        void clearList();
+        void prepend(int value);
+        void append(int value);
+        void insert(int index, int value);
+        int  removeFirst();
+        int  removeLast();
+        int  removeIndex(int index);
+        int  linearSearch(int key) const;
+        int  getElement(int index) const;
+        void setElement(int index, int value);
+        void reverse();
+
+
 };
+
