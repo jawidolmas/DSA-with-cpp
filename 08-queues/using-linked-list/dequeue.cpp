ef27763f3104dfdc5e6ed35c951aa8bf37bb80e2
Jawid Olmas <jawidbay@gmail.com>
2026-08-11 16:42:57 +0300

    Implement dequeue, deleting from queue, with complx. analysis

---
+++ 08-queues/using-linked-list/dequeue.cpp
@@ -0,0 +1,113 @@
+#include<iostream>
+
+
+    /*  QUEUE:
+            Queue using Linked List:
+                Dequeue: Deleting from a Queue
+    */
+
+
+struct node{
+    int data;
+    node *next;
+};
+
+class Queue{
+    private:
+        node *front;
+        node *rear;
+    public:
+        Queue();
+        ~Queue();
+        void enqueu(int x); 
+        int dequeue();
+        void display() const;
+        bool isFull() const;
+        bool isEmpty() const;
+};
+
+// Constructor
+Queue::Queue(){
+    front = rear = nullptr;
+}
+
+// Destructor
+Queue::~Queue(){
+    node *temp = front;
+    while(temp != nullptr){
+        node *next = temp->next;
+        delete temp;
+        temp = next;
+
+    }
+}
+
+// isEmpty
+bool Queue::isEmpty() const{
+    if(front == nullptr){
+        return true;
+    }
+    return false;
+}
+
+// isFull
+bool Queue::isFull() const{
+    node *temp = new node;
+    if(temp == nullptr){
+        return true;
+    }
+    delete temp;
+    temp = nullptr;
+    return false;
+}
+
+// Enqueue: inserting value
+void Queue::enqueu(int x){
+    if(isFull()){
+        std::cout << "Queue is full\n";
+        return;
+    }
+    node *temp = new node;
+    temp->data = x;
+    if(front == nullptr){
+        front = rear = temp;
+        temp->next = nullptr;
+    }else{
+        rear->next = temp;
+        rear = temp;
+    }
+}
+
+// Dequeue: Deleting from Queue
+int Queue::dequeue() {
+    if(isEmpty()){
+        return -1;  // Queue is empty
+    }
+    node *temp = front;
+    front = front->next;
+    int x = temp->data;
+    delete temp;
+    temp = nullptr;
+    return x;
+}
+
+
+
+int main()
+{
+    Queue q1;
+    q1.enqueu(10);
+    q1.enqueu(20);
+    q1.enqueu(30);
+    q1.enqueu(40);
+    q1.dequeue(); // removes 10. 
+
+}
+
+
+        /*  
+            ANALYSIS:
+               Dequeue is O(1) in terms of time and space, which we only delete at first, and move front to next. thats it. 
+
+        
+        */
