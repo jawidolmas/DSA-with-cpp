58034cdc99d5b172a86f1f07cdf1eb0942fe92f6
Jawid Olmas <jawidbay@gmail.com>
2026-08-18 17:21:24 +0300

    Informative notes added for Red-Black Trees

---
+++ 09-trees/binary-trees/BST/Red-Balck-trees/main.cpp
@@ -0,0 +1,8 @@
+
+
+
+/*
+        Red Black trees are type of B-Trees which are similar to BST trees but behaves like 2-3-4 trees.
+        The notes on these topics are available from lectures. check DSA with C++ notes on iPad.
+
+*/
