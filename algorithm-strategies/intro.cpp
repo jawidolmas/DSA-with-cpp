08bd8e0553955244d5c0d2ebbf8830c8cc23d7e3
Jawid Olmas <jawidbay@gmail.com>
2026-08-27 15:14:48 +0300

    Refactored Intro to Algorithm Strategies

---
+++ algorithm-strategies/intro.cpp
@@ -0,0 +1,18 @@
+            
+            
+            
+            
+            
+            /*
+                ALGORITHM STRATEGIES: 
+                    We have strategies to solve problems and detect the pattern. For being able to detect it and solve it, we have strategies and methods. 
+
+                    1. DIVIDE and CONQUER
+
+                    2. GREEDT METHOD
+
+                    3. DYNAMIC PROGRAMMING
+
+                    4. BACKTRACKING
+            */
+           
