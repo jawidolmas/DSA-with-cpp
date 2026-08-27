434d9aacc204b951369baaa3490ed4963f5e54b0
Jawid Olmas <jawidbay@gmail.com>
2026-08-27 12:57:32 +0300

    Introduction to Algorithm Strategies and Divide and Conquer

---
+++ algorithm-strategies/divide-and-conquer/intro.cpp
@@ -0,0 +1,33 @@
+            
+            
+            
+            
+            
+            /*
+                ALGORITHM STRATEGIES: 
+                    1. DIVIDE and CONQUER:
+                            Divide the problem into subproblems and solve it, then combine the solutions for the main problem.
+
+                    THE EXPLNATIONS HAS BEEN TAKEN IN iPAD NOTES.
+
+                    General strucutre:
+                        Divide and Conquer is a recursive method to solve a problem
+
+                        algorithm DAC(p){
+                        
+                        if(small(p)){
+                            solve(p)
+                        }else{
+                        divide into smaller parts;
+                        p1, p2, p3, ... pk;
+                        Apply(DAC(p1, p2, p3...pk));
+                        Combine Solutions(DAC(p1, p2, p3...));
+                        
+                        }
+                    }
+
+
+            */
+
+
+    
